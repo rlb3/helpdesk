8ddfd1ef82fb350948448136b6ba62d3d2606815
Robert Boone <robert@rlb3.com>
2023-03-19 14:40:01 -0500

    config

---
+++ config/config.exs
@@ -0,0 +1,5 @@
+import Config
+
+# if you already have a config file, then you'll just need to add this:
+config :ash, :use_all_identities_in_manage_relationship?, false
+config :helpdesk, :ash_apis, [Helpdesk.Support]
