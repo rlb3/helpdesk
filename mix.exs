b6cf7d34a97ebf961ae5bb271ff19d800eeed307
Robert Boone <robert@rlb3.com>
2023-03-19 14:39:48 -0500

    Add deps

--- mix.exs
+++ mix.exs
@@ -22,6 +22,7 @@ defmodule Helpdesk.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:ash, "~> 2.6.21"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
